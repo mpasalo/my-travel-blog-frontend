8aad2ce0ce2c3bf2e2d15710fdb851ff6f30915f
Marc Pasalo <marcpasalo@gmail.com>
2024-12-05 01:16:51 +0800

    add components

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <div class="grid grid-cols-1 gap-4 place-content-center">
+            <div>
+                <h1 class="text-white text-[40px] text-center mb-4"><em>Travel Blog</em></h1>
+            </div>
+
+        </div>
+        <video autoplay="" loop="" muted id="myVideo">
+            <source src="/highway-loop.mp4" type="video/mp4" />
+        </video>
+    </div>  
+</template>
+<script>
+   
+</script>
---
+++ src/components/RegisterComponent.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="register text-white">
+    <h1 class="text-[40px] text-center mb-4"><em>Register</em></h1>
+
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Name</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    </div>
+
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+  
+    <button @click="registerUser()"  class="btn btn-info">Submit</button>
+</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            form: new Form({
+                name: "",
+                email: "",
+                password: "",
+            })
+        };
+    },
+
+    methods: {
+        registerUser: function() {
+            let formData = new FormData();
+
+            formData.append("name", this.form.name);
+            formData.append("email", this.form.email);
+            formData.append("password", this.form.password);
+
+            let data = {
+                formData: formData
+            };
+
+            this.$store.dispatch("user/register", data);
+        }
+    }
+}
+</script>
