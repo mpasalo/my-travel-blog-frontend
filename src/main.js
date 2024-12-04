4f60371c9d54f6bf7a5d5c53f0638479da01a749
Marc Pasalo <marcpasalo@gmail.com>
2024-12-05 01:17:22 +0800

    add routers

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createWebHistory, createRouter } from 'vue-router'
+import HomeComponent from './components/HomeComponent.vue'
+import LoginComponent from './components/LoginComponent.vue'
+import RegisterComponent from './components/RegisterComponent.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes:[
+        { path: '/', name: 'home', component: HomeComponent },
+        { path: '/login', name: 'login', component: LoginComponent },
+        { path: '/register', name: 'register', component: RegisterComponent }
+    ]
+})
+
+createApp(App)
+.use(router)
+.mount('#app')
