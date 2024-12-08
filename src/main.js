fa9c8ffb42fd569ec1f61f37aff956ccaf8f82d4
Marc Pasalo <marcpasalo@gmail.com>
2024-12-09 00:43:11 +0800

    add navigation guard

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
+router.beforeEach(async (to, from) => {
+    if (
+      // make sure the user is authenticated
+      !store.state.isAuthenticated &&
+      // ❗️ Avoid an infinite redirect
+      to.name !== 'login' && to.name !== 'register' && to.name !== 'home'
+    ) {
+      // redirect the user to the login page
+      return { name: 'login' }
+    }
+})
+
 createApp(App)
 .use(router)
 .use(store)
