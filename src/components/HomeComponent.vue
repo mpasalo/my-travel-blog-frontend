0420514b7bb82383e7fa588c1a3d292b36711a7c
MarcPasalo <marcpasalo@gmail.com>
2025-02-11 01:57:17 +0800

    add arrow icon on home page

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -4,10 +4,14 @@
         <div class="video-content">
             <div class="inner">
                 <h1><span class="your-travel">Your Travel</span> <em>BLOG</em></h1>
-                <p>See the World, One Story at a Time.</p>
-                <!-- <div class="scroll-icon">
-                    <a class="scrollTo" data-scrollTo="portfolio" href="#"><img src="img/scroll-icon.png" alt=""></a>
-                </div>     -->
+                <p class="mb-30">See the World, One Story at a Time.</p>
+                <a href="/posts" class="btn btn-info text-center mt-14">
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                        fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </a>
+                Get Started
             </div>
         </div>
         <video autoplay="" loop="" muted>
